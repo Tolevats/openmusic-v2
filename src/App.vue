cde516498f706118c3178800efad1e478e42699d
Tolevats <pstavelot@gmail.com>
2024-12-13 01:15:33 -0300

    update: Add footer

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,18 @@
     </nav> -->
     <Header />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   components: {
     Header,
+    Footer,
   },
 };
 </script>
