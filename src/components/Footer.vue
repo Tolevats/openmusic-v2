67f99062dd086cc37d281f48c73753de0d0b0dc9
Tolevats <pstavelot@gmail.com>
2024-12-13 01:12:26 -0300

    update: Add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+        <p>&copy; 2024 Tolevats. All rights reserved.</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'footer-component',
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: #1f1f1f;
+  color: white;
+  text-align: center;
+  padding: 1em 0;
+  margin-top: 2em;
+}
+</style>
