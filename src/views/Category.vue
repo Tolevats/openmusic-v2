b7837dabdd31857399276cdf3e7d1352cf2ce84c
Tolevats <pstavelot@gmail.com>
2024-12-14 17:27:10 -0300

    update: Add navbar and better styling

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="category">
-        <h1>{{ category }}</h1>
-        <div class="vinyls">
-            <VinylCard v-for="vinyl in vinyls" :key="vinyl.id" :vinyl="vinyl" />
+        <Navbar />
+        <div class="content">
+            <h1 class="category-title">{{ category }}</h1>
+            <div class="vinyls">
+                <VinylCard v-for="vinyl in vinyls" :key="vinyl.id" :vinyl="vinyl" />
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue';
 import VinylCard from '@/components/VinylCard.vue';
 
 export default {
@@ -19,15 +23,27 @@ export default {
         },
     },
     components: {
+        Navbar,
         VinylCard,
     },
 };
 </script>
 
 <style scoped>
+.content {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.category-title {
+  font-size: 2rem;
+  color: #444;
+  text-align: center;
+  margin: 2rem;
+}
 .vinyls {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+    gap: 10px;
 }
 </style>
